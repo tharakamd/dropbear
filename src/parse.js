ae92ebbf2686b3ad4d6c348df4cb17995fdb53f1
Dilan Tharaka <tharakamd.12@cse.mrt.ac.lk>
2019-11-10 11:43:38 +0530

    parse tests pass

--- src/parse.js
+++ src/parse.js
@@ -2,8 +2,32 @@ const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
 
-const parenthesize = () => {};
+const parenthesize = (tokens) => {return tokens;};
 
-const parse = () => {};
+const parse = (tokens) => {
+  const token = pop(tokens);
+
+  if (token.type === 'Number') {
+    return {
+      type: 'NumericLiteral',
+      value: token.value
+    };
+  }
+
+  if (token.type === 'String') {
+    return {
+      type: 'StringLiteral',
+      value: token.value
+    };
+  }
+
+  if (token.type === 'Name') {
+    return {
+      type: 'Identifier',
+      name: token.value
+    };
+  }
+
+};
 
 module.exports = { parse: tokens => parse(parenthesize(tokens)) };
--- src/tokenize.js
+++ src/tokenize.js
@@ -6,6 +6,77 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+const tokenize = (input) => {
+  const tokens = [];
+  let cursor =0;
+
+  while( cursor < input.length){
+    const character = input[cursor];
+
+    if(isParenthesis(character)){ 
+      tokens.push({
+        type: 'Parenthesis',
+        value: character
+      });
+
+      cursor++;
+      continue;
+    }
+
+    if(isWhitespace(character)){
+      cursor++;
+      continue;
+    }
+
+    if (isNumber(character)) {
+      let number = character;
+
+      while(isNumber(input[++cursor])){
+        number = number + input[cursor];
+      }
+
+      tokens.push({
+        type: 'Number',
+        value: Number(number)
+      });
+      continue;
+    }
+
+    if( isLetter(character)){
+      let symbol = character;
+
+      while (isLetter(input[++cursor])) {
+        symbol = symbol + input[cursor];
+      }
+
+      tokens.push({
+        type: 'Name',
+        value: symbol,
+      });
+
+      continue;
+    }
+
+    if (isQuote(character)) {
+      let quote = '';
+
+      while (!isQuote(input[++cursor])) {
+        quote += input[cursor];
+      }
+
+      tokens.push({
+        type: 'String',
+        value: quote
+      });
+
+      cursor++;
+      continue;
+    }
+
+    cursor++;
+  }
+
+  return tokens;
+};
 
 module.exports = { tokenize };
