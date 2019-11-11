d83b4d61ae7154417ea8399ede868a261b02fe6c
Dilan Tharaka <dilant@codegen.net>
2019-11-11 17:21:19 +0530

    evaluate functions done

--- src/parse.js
+++ src/parse.js
@@ -2,10 +2,36 @@ const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
 
-const parenthesize = (tokens) => {return tokens;};
+const parenthesize = (tokens) => {
+  const token = pop(tokens);
+
+  if (isOpeningParenthesis(token.value)) {
+    const expression = [];
+
+    while (!isClosingParenthesis(peek(tokens).value)) {
+      expression.push(parenthesize(tokens));
+    }
+
+    pop(tokens);
+
+    return expression;
+  }
+
+  return tokens;
+};
 
 const parse = (tokens) => {
-  const token = pop(tokens);
+
+  if (Array.isArray(tokens)) {
+    const [first, ...rest] = [tokens];
+    return {
+      type: 'CallExpression',
+      name: first.value,
+      arguments: rest.map(parse)
+    }
+  }
+
+  const token = tokens;
 
   if (token.type === 'Number') {
     return {
